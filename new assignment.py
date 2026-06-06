984c7a8309ff0316feedf7b2bde1c7f86a86a450
hafsatisah14-arch <hafsatisah14@gmail.com>
2026-06-06 07:32:38 +0100

    Add function to retrieve and display random quotes

---
+++ new assignment.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_random_quote():
+  #  url = "https://api.quotable.io/random"
+  try:
+print("Could not retrieve a quote at this time.")
+print(f"{quote_data["content"]} _ {quote_}") data["aurthur"]},)
+def_init_(self,text,aurthur):
+  self.text = aurthur
+
+def display(self):
+my_quote = quote()
+  print(my_quote.display())
